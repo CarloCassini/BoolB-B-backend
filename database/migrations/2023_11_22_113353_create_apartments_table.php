5b97ff00af18f3635cdf464e541756e0e73a6d3b
CarloCassini <c.cassini87@gmail.com>
2023-11-22 13:58:17 +0100

    creazione model + migration apartments + collegamento 1/N apartmentuser

---
+++ database/migrations/2023_11_22_113353_create_apartments_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->id();
+            // $table->foreignId('user_id')->constrained()->nullOnDelete();
+            $table->string('title', 50);
+            $table->unsignedTinyInteger('rooms');
+            $table->unsignedTinyInteger('beds');
+            $table->unsignedTinyInteger('bathrooms');
+            $table->unsignedInteger('m2');
+            $table->boolean('is_hidden');
+            $table->text('address');
+            $table->text('cover_image_path')->nullable();
+            $table->integer('latitude_int');
+            $table->integer('longitude_int');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+};
