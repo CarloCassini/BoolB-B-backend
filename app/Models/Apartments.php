ff3cb7dfac3e2a4e3f6d84999c99f1383dbeead7
CarloCassini <c.cassini87@gmail.com>
2023-11-22 14:13:36 +0100

    predisposto relazioni tra apartments e le altre tabelle

--- app/Models/Apartments.php
+++ app/Models/Apartments.php
@@ -13,4 +13,32 @@ class Apartments extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    // todo : togliere i commenti quando avremo le tabelle
+    // ** per i messaggi
+    // public function messages()
+    // {
+    //     return $this->hasMany(Messages::class);
+    // }
+
+    // ** per le foto
+    // public function photos() {
+    //     return $this->hasMany(Photo::class);
+    //   }
+
+    // ** per le views
+    // public function views() {
+    //     return $this->hasMany(View::class);
+    //   }
+
+    // !! relazioni molti a molti
+    // ** per i servizi
+    // public function services() {
+    //     return $this->belongsToMany(Service::class);
+    //   }
+
+    // **per gli sponsor
+    // public function sponsors() {
+    //     return $this->belongsToMany(Sponsor::class);
+    //   }
 }
