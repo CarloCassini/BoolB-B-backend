da042584fea847dc9fc1014e15ce7bbe452bbdfc
Pis4ni <pis4wrk@gmail.com>
2023-11-22 15:27:04 +0100

    add: migration create_views_table

---
+++ database/migrations/2023_11_22_135619_create_views_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+
+            //* apartment_id
+            $table->foreignId('apartment_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->datetime('date');
+            $table->text('ip');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('views');
+    }
+};
