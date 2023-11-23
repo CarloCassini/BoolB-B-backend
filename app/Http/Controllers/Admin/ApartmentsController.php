8eaf74a7b477e21aa4f51bb50652e9a210f9440c
Pis4ni <pis4wrk@gmail.com>
2023-11-23 14:46:25 +0100

    fix: restore old controller + preserved show logic

--- app/Http/Controllers/Admin/ApartmentsController.php
+++ app/Http/Controllers/Admin/ApartmentsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 
-use App\Http\Requests\StoreApartmentRequest;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
 
@@ -15,53 +14,49 @@ class ApartmentsController extends Controller
      *
      * *@return \Illuminate\Http\Response
      */
-    public function show(Apartment $apartment)
+    public function index()
     {
-        return view('admin.apartments.show', compact('apartment'));
+        $apartments = Apartment::orderBy('id', 'desc')->paginate(12);
+        return view('admin.apartments.index', compact('apartments'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * *@return \Illuminate\Http\Response
+    //  * @return \Illuminate\Http\Response
      */
     public function create()
     {
-        // return view('admin.apartments.create', compact('apartments'));
-        return view('admin.apartments.create');
+        //
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * *@return \Illuminate\Http\Response
+    //  * @return \Illuminate\Http\Response
      */
-    public function store(StoreApartmentRequest $request)
+    public function store(Request $request)
     {
-        $data = $request->validated();
-        $apartment = new Apartment;
-        $apartment->fill($data);
-
-        //todo -> forso l'inserimento dei campi per vedere il salvataggio
-        $apartment->user_id = 1;
-        $apartment->is_hidden = 0;
-        $apartment->latitude_int = 200;
-        $apartment->longitude_int = 200;
-
-        $apartment->save();
-
-        return view('admin.apartments.show', $apartment);
-
+        //
     }
 
-   
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Apartment  $apartment
+    //  * @return \Illuminate\Http\Response
+     */
+    public function show(Apartment $apartment)
+    {
+        return view('admin.apartments.show', compact('apartment'));
+    }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Apartment  $apartment
-     * @return \Illuminate\Http\Response
+    //  * @return \Illuminate\Http\Response
      */
     public function edit(Apartment $apartment)
     {
@@ -73,7 +68,7 @@ class ApartmentsController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Apartment  $apartment
-     * @return \Illuminate\Http\Response
+    //  * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Apartment $apartment)
     {
@@ -84,7 +79,7 @@ class ApartmentsController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Apartment  $apartment
-     * @return \Illuminate\Http\Response
+    //  * @return \Illuminate\Http\Response
      */
     public function destroy(Apartment $apartment)
     {
