429d94f7a295dfe6f25aee7f2e484bb480137914
CarloCassini <c.cassini87@gmail.com>
2023-12-04 17:03:29 +0100

    add button 4 flow in dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -21,6 +21,32 @@
                             <a class="btn btn-primary" href="{{ route('admin.apartments.index') }}">My Apartments</a>
                             {{-- <a class=" btn btn-success mx-3"
                         href="{{route('admin.messages')}}">My Messages</a> --}}
+
+                            <div class="border my-1">
+                                <a class="my-2 btn btn-warning" href="#">view apartment</a>
+                                <div>
+                                    cliccando un appartamento dalla lista appartamenti deve rendere la vista dello stesso
+                                </div>
+                            </div>
+                            <div class="border my-1">
+                                <a class="my-2 btn btn-warning" href="#">sponsor an apartment(generico)</a>
+                                <div>
+                                    rende la possibilità di collegare una sponsorizzazione legandola a una lista di
+                                    appartamenti specifici
+                                </div>
+                            </div>
+                            <div class="border my-1">
+                                <a class="my-2 btn btn-warning" href="#">apartment statistics(dedicato a un
+                                    apaartamento
+                                    specifico)</a>
+                                <div>
+                                    apre le statistiche dell'appartamento, deve essere legato a una selezione precisa
+                                    dell'appartamento di riferimento
+                                </div>
+                            </div>
+
+
+
                         </div>
                     </div>
                 </div>
