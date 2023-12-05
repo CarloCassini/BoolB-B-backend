70f37023da623bdbdae92cd6da369bfdee448a5d
AlessioMontebello90 <alessio.montebello.am@gmail.com>
2023-12-05 11:08:21 +0100

    add store

---
+++ app/Http/Controllers/Api/MessageController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\MessageController;
+use App\Models\Message;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'sender_email' => 'required|email',
+            'name' => 'nullable|string',
+            'surname' => 'nullable|string',
+            'message' => 'required|string',
+            'apartment_id' => 'required|numeric',
+        ]);
+        // Salvataggio del messaggio nel database
+        $message = new Message();
+        $message->name = $validatedData['name'];
+        $message->surname = $validatedData['surname'];
+        $message->sender_email = $validatedData['sender_email'];
+        $message->message = $validatedData['message'];
+        $message->apartment_id = $validatedData['apartment_id'];
+        $message->save();
+
+        return redirect()->back()->with('success', 'Messaggio inviato con successo!');
+    
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+    
+}
