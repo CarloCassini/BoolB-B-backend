73cb832b6d20188fb66a35478ae1b40249000f77
CarloCassini <c.cassini87@gmail.com>
2023-12-07 23:12:01 +0100

    gestione rotte initialize backend

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Guest;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
-
+use Illuminate\Support\Facades\Auth;
 
 // models usati
 use App\Models\Apartment;
@@ -16,8 +16,33 @@ class PageController extends Controller
 {
   public function index()
   {
-    $apartments = Apartment::orderBy('id', 'desc')->where('is_hidden', '=', '0')->paginate(12);
-    return view('guest.home', compact('apartments'));
+    // $apartments = Apartment::orderBy('id', 'desc')->where('is_hidden', '=', '0')->paginate(12);
+    // return view('guest.home', compact('apartments'));
+    // return view('auth.login');
+
+    if (Auth::user()) {
+
+      $user = Auth::user();
+      $apartments = Apartment::orderBy('id', 'desc')->where('user_id', '=', $user->id)->paginate(12);
+      // fine appartamenti
+
+      // per i messaggi
+      $user_id = $user->id;
+      $messages = [];
+
+      $messagesList = Apartment::join('messages', 'messages.apartment_id', '=', 'apartments.id')
+        ->where('apartments.user_id', '=', $user_id)
+        ->orderBy('messages.created_at', 'desc')->get();
+
+      foreach ($messagesList as $message) {
+        array_push($messages, $message);
+      }
+      // fine messaggi
+
+      return view('admin.dashboard', compact('apartments', 'messages'));
+    } else {
+      return view('auth.login');
+    }
   }
 
   public function show(Apartment $apartment, Request $request, Visualization $visualization)
