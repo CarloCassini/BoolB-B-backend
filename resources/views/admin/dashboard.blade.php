a8c7e41a93878431c801457074d375296fba578a
CarloCassini <c.cassini87@gmail.com>
2023-12-10 12:25:52 +0100

    add rounded to dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -58,7 +58,8 @@
             <div class="row row-cols-2 m-0 ">
                 {{-- appartamenti --}}
                 <nav class="col-12 col-xl-9 navbar navbar-expand-xl ">
-                    <button class=" dashboard-menu d-block d-xl-none navbar-toggler" type="button"
+
+                    <button class=" my-2 dashboard-menu d-block d-xl-none navbar-toggler" type="button"
                         data-bs-toggle="collapse" data-bs-target="#apartmentMenu" aria-controls="apartmentMenu"
                         aria-expanded="false" aria-label="burgher per apartments">
                         <div class=" d-flex justify-content-between align-items-center">
@@ -66,135 +67,137 @@
                             <span class=" ms-auto"><i class="fa-solid fa-bars fa-xl"></i></span>
                         </div>
                     </button>
-                    <div class=" collapse show navbar-collapse" id="apartmentMenu">
-                        <div>
+                    <div class="me-1">
+                        <div class=" collapse show navbar-collapse" id="apartmentMenu">
+                            <div>
+                                <div class="back-btn overflow-auto overflow-x-hidden dashboard-space m-0">
 
-                            <div class="overflow-auto overflow-x-hidden dashboard-space m-0">
-
-                                @if ($apartments[0])
-                                    <table class="table table-striped table-hover m-0">
-                                        <thead>
-                                            <tr>
-                                                <th scope="col">ID</th>
-                                                <th scope="col" class="d-none d-xl-table-cell">cover</th>
-                                                <th scope="col">title</th>
-                                                <th scope="col" class="d-none d-xl-table-cell">rooms</th>
-                                                <th scope="col" class="d-none d-xl-table-cell">beds</th>
-                                                <th scope="col" class="d-none d-xl-table-cell">bathrooms</th>
-                                                <th scope="col" class="d-none d-xl-table-cell">surface</th>
-                                                <th scope='col'>address</th>
-                                                <th scope='col'><i class="fa-solid fa-eye"></i></th>
-                                                <th scope='col'>Actions</th>
-                                                {{-- todo is_hidden must be a btn (as published) --}}
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($apartments as $apartment)
+                                    @if ($apartments[0])
+                                        <table class="table table-striped table-hover m-0">
+                                            <thead>
                                                 <tr>
-                                                    {{-- * id --}}
-                                                    <th scope="row">{{ $apartment->id }}</th>
+                                                    <th scope="col">ID</th>
+                                                    <th scope="col" class="d-none d-xl-table-cell">cover</th>
+                                                    <th scope="col">title</th>
+                                                    <th scope="col" class="d-none d-xl-table-cell">rooms</th>
+                                                    <th scope="col" class="d-none d-xl-table-cell">beds</th>
+                                                    <th scope="col" class="d-none d-xl-table-cell">bathrooms</th>
+                                                    <th scope="col" class="d-none d-xl-table-cell">surface</th>
+                                                    <th scope='col'>address</th>
+                                                    <th scope='col'><i class="fa-solid fa-eye"></i></th>
+                                                    <th scope='col'>Actions</th>
+                                                    {{-- todo is_hidden must be a btn (as published) --}}
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($apartments as $apartment)
+                                                    <tr>
+                                                        {{-- * id --}}
+                                                        <th scope="row">{{ $apartment->id }}</th>
 
-                                                    {{-- * image --}}
-                                                    <td class="d-none d-xl-table-cell">
-                                                        <img class="img-fluid" alt="cover" {{-- controllo sul src delle immagini (3 possibilità) --}}
-                                                            src="@if (!$apartment->cover_image_path) https://via.placeholder.com/2000x1500.png/333333?text=Placeholder
+                                                        {{-- * image --}}
+                                                        <td class="d-none d-xl-table-cell">
+                                                            <img class="img-fluid" alt="cover" {{-- controllo sul src delle immagini (3 possibilità) --}}
+                                                                src="@if (!$apartment->cover_image_path) https://via.placeholder.com/2000x1500.png/333333?text=Placeholder
                                                                 @elseif(Str::startsWith($apartment->cover_image_path, ['http://', 'https://']))
                                                                    {{ $apartment->cover_image_path }}
                                                                 @else
                                                                    {{ asset('/storage/' . $apartment->cover_image_path) }} @endif">
-                                                    </td>
+                                                        </td>
 
-                                                    {{-- * title --}}
-                                                    <td>{{ $apartment->title }}</td>
+                                                        {{-- * title --}}
+                                                        <td>{{ $apartment->title }}</td>
 
-                                                    {{-- * rooms --}}
-                                                    <td class="d-none d-xl-table-cell">{{ $apartment->rooms }}</td>
+                                                        {{-- * rooms --}}
+                                                        <td class="d-none d-xl-table-cell">{{ $apartment->rooms }}</td>
 
-                                                    {{-- * beds --}}
-                                                    <td class="d-none d-xl-table-cell">{{ $apartment->beds }}</td>
+                                                        {{-- * beds --}}
+                                                        <td class="d-none d-xl-table-cell">{{ $apartment->beds }}</td>
 
-                                                    {{-- * bathrooms --}}
-                                                    <td class="d-none d-xl-table-cell">{{ $apartment->bathrooms }}</td>
+                                                        {{-- * bathrooms --}}
+                                                        <td class="d-none d-xl-table-cell">{{ $apartment->bathrooms }}</td>
 
-                                                    {{-- * m2 --}}
-                                                    <td class="d-none d-xl-table-cell"><span
-                                                            class="text-nowrap">{{ $apartment->m2 }} &#13217</span>
-                                                    </td>
+                                                        {{-- * m2 --}}
+                                                        <td class="d-none d-xl-table-cell"><span
+                                                                class="text-nowrap">{{ $apartment->m2 }} &#13217</span>
+                                                        </td>
 
-                                                    {{-- * address --}}
-                                                    <td>{{ $apartment->address }}</td>
+                                                        {{-- * address --}}
+                                                        <td>{{ $apartment->address }}</td>
 
-                                                    {{-- * is_hidden --}}
-                                                    <td>
-                                                        @if ($apartment->is_hidden == '0')
-                                                            <i class="fa-solid fa-square-check text-success"></i>
-                                                        @else
-                                                            <i class="fa-solid fa-square-xmark text-danger"></i>
-                                                        @endif
-                                                    </td>
+                                                        {{-- * is_hidden --}}
+                                                        <td>
+                                                            @if ($apartment->is_hidden == '0')
+                                                                <i class="fa-solid fa-square-check text-success"></i>
+                                                            @else
+                                                                <i class="fa-solid fa-square-xmark text-danger"></i>
+                                                            @endif
+                                                        </td>
 
-                                                    {{-- * actions buttons --}}
-                                                    {{-- todo da creare le rotte corrette --}}
+                                                        {{-- * actions buttons --}}
+                                                        {{-- todo da creare le rotte corrette --}}
 
-                                                    <td class="h-100">
-                                                        <div
-                                                            class="h-100 d-flex align-items-center justify-content-between">
+                                                        <td class="h-100">
+                                                            <div
+                                                                class="h-100 d-flex align-items-center justify-content-between">
 
-                                                            <a href="{{ route('admin.apartments.show', $apartment) }}"
-                                                                class="mx-1"><i
-                                                                    class="fa-solid fa-circle-info text-primary"></i></a>
-                                                            <a href="{{ route('admin.apartments.edit', $apartment) }}"
-                                                                class="mx-1"><i
-                                                                    class="fa-solid fa-pencil text-warning"></i></a>
+                                                                <a href="{{ route('admin.apartments.show', $apartment) }}"
+                                                                    class="mx-1"><i
+                                                                        class="fa-solid fa-circle-info text-primary"></i></a>
+                                                                <a href="{{ route('admin.apartments.edit', $apartment) }}"
+                                                                    class="mx-1"><i
+                                                                        class="fa-solid fa-pencil text-warning"></i></a>
 
-                                                            <a href="{{ route('show.statistics', $apartment->id) }}"
-                                                                class="mx-1"><i
-                                                                    class="fa-solid fa-chart-simple text-info"></i></a>
+                                                                <a href="{{ route('show.statistics', $apartment->id) }}"
+                                                                    class="mx-1"><i
+                                                                        class="fa-solid fa-chart-simple text-info"></i></a>
 
-                                                            <a href="{{ route('sponsorSelect', $apartment->id) }}"
-                                                                class="mx-1"><i
-                                                                    class="fa-solid fa-money-check-dollar text-success"></i></a>
+                                                                <a href="{{ route('sponsorSelect', $apartment->id) }}"
+                                                                    class="mx-1"><i
+                                                                        class="fa-solid fa-money-check-dollar text-success"></i></a>
 
-                                                            <a href="#"data-bs-toggle="modal"
-                                                                data-bs-target="#modal-{{ $apartment->id }}"
-                                                                class="mx-1"><i
-                                                                    class="fa-solid fa-trash text-danger"></i></a>
-                                                        </div>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
-                                @else
-                                    <h1> No Apartments Found :( </h1>
-                                @endif
-                            </div>
-                            <div class="container dashboard-head overflow-hidden">
-                                <div class=" text-gradient">
-                                    {{-- se esiste almeno un oggetto in apartment --}}
-                                    <div class="my-1 py-2 dashboard-pagination">
-                                        {{ $apartments->links('pagination::bootstrap-5') }}
-                                    </div>
+                                                                <a href="#"data-bs-toggle="modal"
+                                                                    data-bs-target="#modal-{{ $apartment->id }}"
+                                                                    class="mx-1"><i
+                                                                        class="fa-solid fa-trash text-danger"></i></a>
+                                                            </div>
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    @else
+                                        <h1> No Apartments Found :( </h1>
+                                    @endif
                                 </div>
-                                <hr class="m-0">
+                                <div class="back-btn mt-2 container dashboard-head overflow-hidden">
+                                    <div class=" text-gradient">
+                                        {{-- se esiste almeno un oggetto in apartment --}}
+                                        <div class="my-1 py-2 dashboard-pagination">
+                                            {{ $apartments->links('pagination::bootstrap-5') }}
+                                        </div>
+                                    </div>
+                                    <hr class="m-0">
+                                </div>
                             </div>
                         </div>
+
                     </div>
                 </nav>
                 {{-- messaggi --}}
-                <nav class="col-12 col-xl-3 navbar navbar-expand-xl ">
-                    <button class="dashboard-menu d-block d-xl-none navbar-toggler" type="button" data-bs-toggle="collapse"
-                        data-bs-target="#msgMenu" aria-controls="msgMenu" aria-expanded="false"
+                <nav class="col-12 col-xl-3 navbar navbar-expand-xl mb-3 ">
+                    <button class=" my-2 dashboard-menu d-block d-xl-none navbar-toggler" type="button"
+                        data-bs-toggle="collapse" data-bs-target="#msgMenu" aria-controls="msgMenu" aria-expanded="false"
                         aria-label="burgher per apartments">
                         <div class=" d-flex justify-content-between align-items-center">
                             <h3>messages table</h3>
                             <span class=" ms-auto"><i class="fa-solid fa-bars fa-xl"></i></span>
                         </div>
                     </button>
-                    <div class=" collapse navbar-collapse" id="msgMenu">
+                    <div class=" mb-3 collapse navbar-collapse" id="msgMenu">
                         <div>
-                            <div class="container dashboard-head overflow-hidden mt-4 ">
-                                <div class="d-block text-gradient">
+                            <div class="container dashboard-head overflow-hidden my-2 back-btn ">
+                                <div class=" text-gradient">
                                     <div class=" dashboard-head d-flex justify-content-between align-items-center">
                                         <h4>My messages</h4>
                                         <a class=" btn btn-primary me-3"
@@ -203,8 +206,7 @@
                                 </div>
                                 <hr class="m-0 mt-auto">
                             </div>
-                            <div class="overflow-scroll overflow-x-hidden dashboard-space m-0 ">
-
+                            <div class=" back-btn overflow-scroll overflow-x-hidden dashboard-space m-0 ">
                                 <table class="table table-striped table-hover">
                                     <tbody>
                                         @foreach ($messages as $message)
